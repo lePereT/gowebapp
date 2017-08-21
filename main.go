413f8ecd30946945d25873618490ce2b30734521
lePereT <richardthanki@gmail.com>
2017-08-21 11:04:29 +0100

    more first commits

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ import (
 var assetsPath string
 
 func processFlags() *daemon.Config {
-	cfg := &daemon.Config{} // cfg is a pointer to daemon.Config
+	cfg := &daemon.Config{} // cfg is a pointer to a daemon.Config struct as defined in the other package
 
+
+	/* these first two statements take the command line arguments and 
+	associate them to pre-defined variables in daemon.Config. The third 
+	associates a flag with the assetsPath declared earlier. In all cases   
+	need to pass pointers to the flag declaration function (mutates) */
 	flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
 	flag.StringVar(&cfg.Db.ConnectString, "db-connect", "host=/var/run/postgresql dbname=gowebapp sslmode=disable", "DB Connect String")
 	flag.StringVar(&assetsPath, "assets-path", "assets", "Path to assets dir")
